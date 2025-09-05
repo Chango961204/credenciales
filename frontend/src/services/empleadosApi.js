f843494c62b23aa8158ef0c0a050ab9ba0864a31
Chango961204 <metallicaluis73@gmail.com>
2025-09-05 14:30:34 -0600

    arreglos frontend

--- frontend/src/services/empleadosApi.js
+++ frontend/src/services/empleadosApi.js
@@ -5,7 +5,7 @@ const API_URL = "http://localhost:4000/api/empleados";
 export const importarExcel = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
-  return await axios.post(`${API_URL}/import`, formData);
+  return await axios.post(`${API_URL}/importar`, formData);
 };
 export const obtenerEmpleados = async (page = 1, limit = 10) => {
   const res = await axios.get("http://localhost:4000/api/empleados", {
